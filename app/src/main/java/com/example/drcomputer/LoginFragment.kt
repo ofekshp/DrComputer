6121771afaa8fbc53c9f6b5d3073b7b2913de2d0
Ofek Shpirer <ofekshp2821@gmail.com>
2024-05-07 18:21:00 +0300

    Auth - Try#1

--- app/src/main/java/com/example/drcomputer/LoginFragment.kt
+++ app/src/main/java/com/example/drcomputer/LoginFragment.kt
@@ -1,11 +1,17 @@
 package com.example.drcomputer
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.drcomputer.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
 
@@ -14,7 +20,37 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val emailText: TextInputEditText = view.findViewById(R.id.emailLog)
+        val passwordText: TextInputEditText = view.findViewById(R.id.passwordLog)
+        val btnLog: Button = view.findViewById(R.id.btn_login)
+        var auth: FirebaseAuth = FirebaseAuth.getInstance()
+        btnLog.setOnClickListener{
+            val email: String
+            val password: String
+            email = emailText.text.toString()
+            password = passwordText.text.toString()
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(context, "Enter Email", Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(context, "Enter Password", Toast.LENGTH_SHORT).show()
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener{ task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(context,"Login Success",Toast.LENGTH_SHORT).show()
+                        //val intent:Intent = Intent(applicationContext, LoginActivity::class.java)
+                    } else {
+                        Toast.makeText(context,"Login Failed",Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+
+        return view
     }
 
 }
--- app/src/main/java/com/example/drcomputer/RegisterFragment.kt
+++ app/src/main/java/com/example/drcomputer/RegisterFragment.kt
@@ -1,24 +1,53 @@
 package com.example.drcomputer
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.drcomputer.R
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class RegisterFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view =  inflater.inflate(R.layout.fragment_register, container, false)
-        val emailText: TextInputEditText
-        val passwordText: TextInputEditText
-        emailText = view.findViewById(R.id.emailReg)
-        passwordText = view.findViewById(R.id.passwordReg)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+        val emailText: TextInputEditText = view.findViewById(R.id.emailReg)
+        val passwordText: TextInputEditText  = view.findViewById(R.id.passwordReg)
+        val btnReg: Button = view.findViewById(R.id.btn_register)
+        var auth:FirebaseAuth = FirebaseAuth.getInstance()
+
+
+        btnReg.setOnClickListener {
+            val email: String
+            val password: String
+            email = emailText.text.toString()
+            password = passwordText.text.toString()
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(context, "Enter Email", Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(context, "Enter Password", Toast.LENGTH_SHORT).show()
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener{ task ->
+                    if (task.isSuccessful) {
+                       Toast.makeText(context,"Register Success",Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(context,"Register Failed",Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
 
 
         return view;
